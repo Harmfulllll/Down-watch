8944febe2a5779cc9122d5dd9b51cf61b83c1bd7
Harmfullllll <tanvirjoy114@gmail.com>
2024-08-03 20:57:50 +0600

    Added sites redux

---
+++ app/src/Hooks/useGetSites.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { getSites } from "../redux/siteSlice.js";
+
+function useGetSites() {
+  const [sitesLoading, setSitesLoading] = useState(false);
+  const dispatch = useDispatch();
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  };
+  const getAllSites = async () => {
+    try {
+      const res = await axios.get("/api/v1/site/get-all", {}, config);
+      if (res.error) throw new Error(res.error);
+      dispatch(getSites(res?.data));
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "An error occurred");
+    } finally {
+      setSitesLoading(false);
+    }
+  };
+  return { sitesLoading, getAllSites };
+}
+export default useGetSites;
--- app/src/components/DashboardBody/DashboardBody.jsx
+++ app/src/components/DashboardBody/DashboardBody.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 import "./DashboardBody.css";
+import { useSelector } from "react-redux";
+import useGetSites from "../../Hooks/useGetSites";
 
 function DashboardBody() {
+  const { sitesLoading, getAllSites } = useGetSites();
+  React.useEffect(() => {
+    getAllSites();
+  }, []);
+  let sites = useSelector((state) => state.site);
+  sites = sites.site;
+
+  if (sites === null) sites = [];
+
   return (
     <div className="dashboard-body">
       <h1>Site status</h1>
 
       <div className="site-items">
-        <div className="site-item">
-          <p className="website-name">Website 1</p>
-          <div className="status">
-            <img src="/green.png" alt="up" />
-            <p>Down</p>
-          </div>
-          <button>Delete</button>
-        </div>
-        <div className="site-item">
-          <p className="website-name">Website 1</p>
-          <div className="status">
-            <img src="/green.png" alt="up" />
-            <p>Down</p>
-          </div>
-          <button>Delete</button>
-        </div>
+        {sites?.data?.length > 0 &&
+          sites?.data[0]?.sites?.map(
+            (site) => (
+              console.log(site),
+              (
+                <div className="site-item" key={site?._id}>
+                  <p className="website-name">{site?.url}</p>
+                  <div className="status">
+                    <img
+                      src={site?.status === "up" ? "/green.png" : "/red.png"}
+                      alt={site?.status}
+                    />
+                    <p>{site?.status}</p>
+                  </div>
+                  <button>Delete</button>
+                </div>
+              )
+            )
+          )}
+        {sites.length === null && <p>No sites added</p>}
       </div>
     </div>
   );
