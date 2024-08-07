b2d5a1c03d418391db1848bb65515d7e571f39cb
Harmfullllll <tanvirjoy114@gmail.com>
2024-08-07 22:30:04 +0600

    Update target URL for API proxy in vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://down-watch.vercel.app",
+        target: "https://down-watch.onrender.com",
       },
     },
   },
