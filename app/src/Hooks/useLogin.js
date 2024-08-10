e73e1bfae816c66941b5b642b1fb9ef05b2aa143
Harmfullllll <tanvirjoy114@gmail.com>
2024-08-10 13:37:45 +0600

    Update API endpoint URL for user login and fix CORS configuration

--- app/src/Hooks/useLogin.js
+++ app/src/Hooks/useLogin.js
@@ -18,7 +18,7 @@ function useLogin() {
     try {
       setLoading(true);
       const res = await axios.post(
-        "https://down-watch.onrender.com/api/v1/user/login",
+        "https://down-watch.vercel.app/api/v1/user/login",
         {
           email,
           password,
